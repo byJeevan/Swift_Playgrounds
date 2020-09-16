1a299c2af6167a4483f8cb6c99c8e9e92b53fcf0
jeevan-1382 <a_jeevan.rao@africa.airtel.com>
2020-09-16 22:43:20 +0530

    Design Patterns/ theme builder

---
+++ Swift Playgrounds/Design Patterns/Builder Pattern - Theme.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+
+///- Builder pattern : Theme builder.
+protocol ThemeProtocol {
+    var backgroundColor:UIColor? { get }
+    var textColor:UIColor? { get }
+}
+
+class Theme : ThemeProtocol {
+    var backgroundColor:UIColor?
+    var textColor:UIColor?
+    typealias buildThemeClosure = (Theme) -> Void
+
+    init(build:buildThemeClosure) {
+        build(self)
+    }
+}
+
+let darkTheme = Theme(build: {
+    $0.backgroundColor = UIColor.black
+    $0.textColor = UIColor.white
+})
+
+let lightTheme = Theme(build: {
+    $0.backgroundColor = UIColor.white
+    $0.textColor = UIColor.black
+})
+
+///usage in some were in your code
+darkTheme.textColor
+lightTheme.backgroundColor
+
