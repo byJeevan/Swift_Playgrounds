0b55362bd753409a32bda32418f0265285026afa
Jeevan <jeevan8082@gmail.com>
2021-02-10 21:56:06 +0530

    Update Contents.swift

--- Swift Playgrounds/Algorithms/Sorting/Insertion Sort.playground/Contents.swift
+++ Swift Playgrounds/Algorithms/Sorting/Insertion Sort.playground/Contents.swift
@@ -1,5 +1,7 @@
 /************************
- Insertion Sort
+ Insertion Sort :
+ The array is virtually split into a sorted and an unsorted part. Values from the unsorted part are picked 
+ and placed at the correct position in the sorted part.
  ************************/
 
 import Foundation
